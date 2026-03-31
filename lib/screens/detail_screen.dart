ef0840b2c4549c1621651d0459176e4381132c77
Alfin831 <alfinnn831@gmail.com>
2026-03-31 20:36:57 +0700

    final detail

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,49 +1,106 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_daftar_movie/models/movie.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class DetailScreen extends StatelessWidget {
+class DetailScreen extends StatefulWidget {
   final Movie movie;
-
   const DetailScreen({super.key, required this.movie});
+  @override
+  State<DetailScreen> createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
+  bool _isFavorite = false;
+  Future<void> _checkIsFavorite() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _isFavorite = prefs.containsKey('movie_${widget.movie.id}');
+    });
+  }
+
+  Future<void> _toggleFavorite() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _isFavorite = !_isFavorite;
+    });
+    if (_isFavorite) {
+      final String movieJson = jsonEncode(widget.movie.toJson());
+      prefs.setString('movie_${widget.movie.id}', movieJson);
+      List<String> favoriteMovieIds =
+          prefs.getStringList('favoriteMovies') ?? [];
+      favoriteMovieIds.add(widget.movie.id.toString());
+      prefs.setStringList('favoriteMovies', favoriteMovieIds);
+    } else {
+      prefs.remove('movie_${widget.movie.id}');
+      List<String> favoriteMovieIds =
+          prefs.getStringList('favoriteMovies') ?? [];
+      favoriteMovieIds.remove(widget.movie.id.toString());
+      prefs.setStringList('favoriteMovies', favoriteMovieIds);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(movie.title)),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Image.network(
-              'https://image.tmdb.org/t/p/w500${movie.posterPath}',
-              width: double.infinity,
-              height: 300,
-              fit: BoxFit.cover,
+      appBar: AppBar(title: Text(widget.movie.title)),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Image.network(
+            widget.movie.backdropPath != ''
+                ? 'https://image.tmdb.org/t/p/w500${widget.movie.backdropPath}'
+                : 'https://via.placeholder.com/500?text=No+Image',
+            height: 300,
+            width: double.infinity,
+            fit: BoxFit.cover,
+          ),
+
+          Positioned(
+            top: 10,
+            right: 10,
+            child: IconButton(
+              onPressed: _toggleFavorite,
+              icon: _isFavorite
+                  ? const Icon(Icons.favorite, color: Colors.red)
+                  : const Icon(Icons.favorite_border),
             ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    movie.title,
-                    style: const TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const SizedBox(height: 8),
-                  Text(
-                    'Rating: ${movie.voteAverage}',
-                    style: const TextStyle(fontSize: 16),
-                  ),
-                  const SizedBox(height: 16),
-                  Text(movie.overview, style: const TextStyle(fontSize: 14)),
-                ],
+          ),
+
+          const SizedBox(height: 20),
+          const Text(
+            'Overview:',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 10),
+          Text(widget.movie.overview),
+          const SizedBox(height: 20),
+          Row(
+            children: [
+              const Icon(Icons.calendar_month, color: Colors.blue),
+              const SizedBox(width: 10),
+              const Text(
+                'Release Date:',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
               ),
-            ),
-          ],
-        ),
+              const SizedBox(width: 10),
+              Text(widget.movie.releaseDate),
+            ],
+          ),
+          const SizedBox(height: 20),
+          Row(
+            children: [
+              const Icon(Icons.star, color: Colors.amber),
+              const SizedBox(width: 10),
+              const Text(
+                'Rating:',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(width: 10),
+              Text(widget.movie.voteAverage.toString()),
+            ],
+          ),
+        ],
       ),
     );
   }
